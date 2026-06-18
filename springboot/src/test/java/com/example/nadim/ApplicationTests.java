72c99c530fd5484f417303c5b100978acf9435c1
eitnadim <nadim@ethicalintelligent.com>
2026-06-18 16:10:27 +0530

    Add springboot/src/test/java/com/example/nadim/ApplicationTests.java

---
+++ springboot/src/test/java/com/example/nadim/ApplicationTests.java
@@ -0,0 +1,6 @@
+package com.example.nadim;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest class ApplicationTests {
+    @Test void contextLoads() {}
+}
